9d2be5d614a8537f0a5d0f3bb71c7f858ce15d40
Ismail Jalloh <ijalloh21@gmail.com>
2019-08-19 22:54:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,9 +29,11 @@ class Transfer
   
   
   def reverse_transfer
-    @receiver.balance -= @amount
-    @sender.balance += @amount
-    @status = "reversed"
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
   end
   
   
