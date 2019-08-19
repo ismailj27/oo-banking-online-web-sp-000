88d01f34d945d0572c725b383dce83636151af62
Ismail Jalloh <ijalloh21@gmail.com>
2019-08-19 23:04:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @sender.balance < @amount && @sender.valid?
+    elsif @sender.balance < @amount && @sender.valid? && @status == "pending"
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
