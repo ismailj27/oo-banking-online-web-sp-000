0581e6f3365edcfc93d058a847eb274caf97cfb5
Ismail Jalloh <ijalloh21@gmail.com>
2019-08-19 22:57:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid? && @status == "pending"
+    if @sender.balance < 0 && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
